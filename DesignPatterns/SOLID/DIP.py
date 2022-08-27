e0a30b97a44aa18561e55a1f157c6d2bed794cb4
dev-siv <sivasai10848@gvpce.ac.in>
2022-08-28 00:49:15 +0530

    DIP code

--- DesignPatterns/SOLID/DIP.py
+++ DesignPatterns/SOLID/DIP.py
@@ -30,18 +30,27 @@ class Relationships(RelationshipBrower):
 
         )
 
+    def find_all_children_of(self, name):
+        for r in self.relations:
+            if r[0].name == name and r[1] == Realtionship.PARENT:
+                yield r[2]
+
 
 class Research:
     # Research is a high-level module that works with the low-level modules
     # and exposes a simple interface to the client.
     # The client should not be forced to depend on the low-level modules directly.
     # Research should work with any implementation of the low-level modules.
-    # 
+    # So instead of depending on the low level module it depennded on the abstractmethod
+    # defined in the abstract method.
     # def __init__(self, relationships):
     #     self.relations = relationships.relations
     #     for r in self.relations:
     #         print(f"{r[0].name} is {r[1].name} of {r[2].name}")
-        
+    def __init__(self, browser) -> None:
+        for p in browser.find_all_children_of("John"):
+            print(f"John has a child called {p.name}")
+
 
    
 
